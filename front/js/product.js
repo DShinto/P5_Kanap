ba0f7897574feca36bc8244b750a4cf5c3bd2650
damien <damienluthy@gmail.com>
2022-09-29 09:11:16 +0200

    pages accueil et produit dynamiques

--- front/js/product.js
+++ front/js/product.js
@@ -1,1 +1,57 @@
+// Récupération de la chaine de requete dans l'url
+const requeteUrlId = window.location.search;
 
+// Méthode URLSearchParams
+const UrlCanape = new URLSearchParams(requeteUrlId);
+
+//variable contenant l'Id
+const canapeId = UrlCanape.get("id");
+
+//Fonction asynchrone pour appeler l'api/products/id
+async function getCanapesId() {
+    const response = await fetch(
+        "http://localhost:3000/api/products/" + canapeId
+    );
+    // Condition si on atteint bien l'API
+    if (response.ok) {
+        return await response.json();
+    } else {
+        console.error("retour du serveur : ", response.status);
+    }
+}
+
+// Générer les éléments de la page produit
+function genererProductId(data) {
+    const imageProductId = document.querySelector(".item__img");
+    imageProductId.innerHTML += `<img src="${data.imageUrl}" alt="${data.altTxt}">`;
+
+    const titleId = document.querySelector("#title");
+    titleId.innerHTML += `<h1 id="title">${data.name}</h1>`;
+
+    const priceId = document.querySelector("#price");
+    priceId.innerHTML += `<span id="price">${data.price}</span>`;
+
+    const descriptionId = document.querySelector("#description");
+    descriptionId.innerHTML += ` <p id="description">${data.description}</p>`;
+
+    // Génération des couleurs à l'aidde d'une boucle pour récupérer toutes les couleurs
+    const colorsId = document.querySelector("#colors");
+    for (let i = 0; i < data.colors.length; i++)
+        colorsId.innerHTML += `<option value="${data.colors[i]}">${data.colors[i]}</option>`;
+}
+
+// Appel de la promesse et génération de la fiche
+getCanapesId().then((data) => genererProductId(data));
+
+function addCart() {
+    const id = canapeId;
+    console.log(id);
+    const color = document.querySelector("#colors").value;
+    console.log(color);
+    const quantity = document.querySelector("#quantity").value;
+    console.log(quantity);
+}
+
+const buttonAddCart = document.querySelector("#addToCart");
+
+buttonAddCart.addEventListener("click", addCart);
--- front/js/script.js
+++ front/js/script.js
@@ -1,35 +1,45 @@
+// Récupération des données de l'API
 async function getCanapes() {
-  const response = await fetch("http://localhost:3000/api/products");
-  if (response.ok) {
-    return await response.json();
-  } else {
-    console.error("retour du serveur : ", reponse.status);
-  }
+    const response = await fetch("http://localhost:3000/api/products");
+    // Condition si on atteint bien l'API
+    if (response.ok) {
+        return await response.json();
+    } else {
+        console.error("retour du serveur : ", response.status);
+    }
 }
 
+// Création des fiches canapés selon le code HTML
 function genererFiches(data) {
-  for (let i = 0; i < data.length; i++) {
-    const items = document.querySelector(".items");
-    const productItems = document.createElement("a");
-    productItems.href = `product.html?id=${data[i]._id}`;
+    // Récupération des articles de l'API
+    for (let i = 0; i < data.length; i++) {
+        // Création des cartes produits
+        const items = document.querySelector(".items");
 
-    const productArticle = document.createElement("article");
-    productItems.appendChild(productArticle);
+        //Création de la balise <a> pour faire le lien avec la page produit
+        const productItems = document.createElement("a");
+        productItems.href = `product.html?id=${data[i]._id}`;
 
-    const productImage = document.createElement("img");
-    productImage.src = data[i].imageUrl;
-    productImage.alt = data[i].altTxt;
-    productArticle.appendChild(productImage);
+        // Création de l'article et des différentes balises
+        const productArticle = document.createElement("article");
+        productItems.appendChild(productArticle);
 
-    const productName = document.createElement("h3");
-    productName.innerText = data[i].name;
-    productArticle.appendChild(productName);
+        const productImage = document.createElement("img");
+        productImage.src = data[i].imageUrl;
+        productImage.alt = data[i].altTxt;
+        productArticle.appendChild(productImage);
 
-    const productDescription = document.createElement("p");
-    productDescription.innerText = data[i].description;
-    productArticle.appendChild(productDescription);
+        const productName = document.createElement("h3");
+        productName.innerText = data[i].name;
+        productArticle.appendChild(productName);
 
-    items.appendChild(productItems);
-  }
+        const productDescription = document.createElement("p");
+        productDescription.innerText = data[i].description;
+        productArticle.appendChild(productDescription);
+
+        items.appendChild(productItems);
+    }
 }
+
+// Appel des fiches
 getCanapes().then((data) => genererFiches(data));
